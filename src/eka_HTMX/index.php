902b85beeb32e31a77070bacd5ecf7cdb9613148
henkkapoika <henriknerg@hotmail.com>
2024-08-06 19:37:10 +0300

    koulupvä2

--- src/eka_HTMX/index.php
+++ src/eka_HTMX/index.php
@@ -1,9 +1,13 @@
+<?php
+include 'funcs.php';
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>eka HTMX</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://unpkg.com/htmx.org@2.0.1" integrity="sha384-QWGpdj554B4ETpJJC9z+ZHJcA/i59TyjxEPXiiUgN2WmTyV5OEZWCD6gQhgkdpB/" crossorigin="anonymous"></script>
 </head>
 <body>
     <header id="main-header">
@@ -12,7 +16,15 @@
     </header>
     <main>
         <p>You can build modern user interfaces with the simplicity and power of hypertext</p>
-        <button>Learn more</button>
+        <form hx-post="note.php">
+            <label for="note">Your note</label>
+            <input type="text" id="note" name="note">
+            <button>Save Button</button>
+        </form>
+        <?php //generateList(); ?>
+        <?php include 'info.php'; ?>
+        <!--<button hx-get="info.php" hx-trigger="mouseenter[ctrlKey] once, click" hx-target="main" hx-swap="beforeend">Learn more</button>
+        -->
     </main>
 </body>
 </html>
